6f86b056c4d8dc0e2b65053c0ff962702ccdd157
rishishukla042 <rishishukla042@gmail.com>
2019-02-27 23:12:17 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WebApp';
+@ViewChild('f') signup: NgForm;
+user = {
+  UserName: '',
+  Email : '',
+  Contact : ''
+};
+
+submitted = false;
+  
+onSubmit(){
+   this.submitted = true;
+   this.user.UserName = this.signup.value.Name;
+   this.user.Email = this.signup.value.email;
+   this.user.Contact = this.signup.value.cell;
+  }
 }
